1c1039f7b65aed5a9346f9f0dcc0b1260786b53c
bgaba <>
2024-08-31 23:24:04 +0200

    Use the standardized errors

--- cmd/api/movie.go
+++ cmd/api/movie.go
@@ -1,10 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "show movie handler works!")
+	id, err := app.readIDParam(r)
+	if err != nil {
+		app.badRequestError(w,r,err)
+		return
+	}
+	app.logger.Printf("id: %d provided", id)
+
 }
--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -11,6 +11,9 @@ func (app *application) routes() http.Handler {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
+	
+	r.NotFound(app.notFoundError)
+	r.MethodNotAllowed(app.methodNotAllowed)
 
 	apiRouter := chi.NewRouter()
 	apiRouter.Get("/movies/{id}", app.showMovieHandler)
