22c0d58691aed46b7f47419548929de67dcdec75
bgaba <>
2024-08-31 22:18:54 +0200

    add a router to the server api app

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -54,6 +54,7 @@ func main() {
 func (app *application) run() error {
 	srv := http.Server{
 		Addr: fmt.Sprintf(":%d", app.config.port),
+		Handler: app.routes(),
 		IdleTimeout: time.Minute,
 		ReadTimeout: 10 * time.Second,
 		WriteTimeout: 30 * time.Second,
---
+++ cmd/api/routes.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func (app *application) routes() http.Handler {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	apiRouter := chi.NewRouter()
+
+	r.Mount("/api/v1", apiRouter)
+
+	return r
+}
