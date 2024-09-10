f6c4e4bec6b7ed184d1192b8b70a51489ebb648a
bgaba <>
2024-09-10 08:21:48 +0200

    add movie filtering by title and genres

--- internal/data/movie.go
+++ internal/data/movie.go
@@ -128,12 +128,16 @@ func (m *MovieModel) GetAll(title string, genres[]string, filters Filters) ([]*M
 	query := `
 	SELECT id, title, year, runtime, genres, version
 	FROM movies
+	WHERE (LOWER(title) = LOWER($1) OR $1 = '')
+	AND (genres @> $2 OR $2 = '{}')
 	ORDER BY id
 	`
 	ctx, cancel := context.WithTimeout(context.Background(), 3 * time.Second)
 	defer cancel()
 
-	rows, err := m.DB.QueryContext(ctx, query)
+	args := []any{title, pq.Array(genres)}
+
+	rows, err := m.DB.QueryContext(ctx, query, args...)
 	if err != nil {
 		return nil, err
 	}
